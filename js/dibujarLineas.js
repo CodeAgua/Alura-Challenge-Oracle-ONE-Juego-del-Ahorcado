ee8d60cc6cbf2de4392560d5ba82dc6770651710
WaterCode <96397960+CodeAgua@users.noreply.github.com>
2022-07-17 02:27:47 -0300

    Registro correcto del input del teclado
    Las teclas presionadas quedan registradas en el programa.

--- js/dibujarLineas.js
+++ js/dibujarLineas.js
@@ -1,18 +1,81 @@
-import { tablero } from "./index.js";
-import { palabraSecreta } from "./index.js";
+import { tablero, palabraSecreta, letras } from "./index.js";
+
+export let errores = 8;
+export let palabraCorrecta = "";
 
 export function dibujarLineas() {
-    tablero.lineWidth = 6;
-    tablero.lineCap = "round";
-    tablero.lineJoin = "round";
-    tablero.strokeStyle = "#b746f0";
-    tablero.beginPath();
+  tablero.lineWidth = 6;
+  tablero.lineCap = "round";
+  tablero.lineJoin = "round";
+  tablero.strokeStyle = "#b746f0";
+  tablero.beginPath();
 
-    var ancho = 600 / palabraSecreta.length;
-    for (let i = 0; i < palabraSecreta.length; i++) {
-        tablero.moveTo(500 + (ancho * i), 640);
-        tablero.lineTo(550 + (ancho * i), 640);
-    }
-    tablero.stroke();
-    tablero.closePath();
+  var ancho = 600 / palabraSecreta.length;
+  for (let i = 0; i < palabraSecreta.length; i++) {
+    tablero.moveTo(500 + ancho * i, 640);
+    tablero.lineTo(550 + ancho * i, 640);
+  }
+  tablero.stroke();
+  tablero.closePath();
 }
+
+export function dibujarLetrasCorrectas(index) {
+  tablero.font = "bold 52px 'Inter'";
+  tablero.lineWidth = 6;
+  tablero.lineCap = "round";
+  tablero.lineJoin = "round";
+  tablero.fillStyle = "#b746f0";
+
+  var ancho = 600 / palabraSecreta.length;
+  tablero.fillText(palabraSecreta[index], 505 + ancho * index, 620);
+}
+
+export function dibujarLetrasIncorrectas(letra, errorsLeft) {
+  tablero.font = "bold 40px Inter";
+  tablero.lineWidth = 6;
+  tablero.lineCap = "round";
+  tablero.lineJoin = "round";
+  tablero.fillStyle = "#b746f0";
+
+  tablero.fillText(letra, 535 + 40 * (10 - errorsLeft), 710, 40);
+}
+
+export function verificarLetraApretada(key) {
+  if (letras.lenght < 1 || letras.indexOf(key) < 0) {
+    letras.push(key);
+    return false;
+  } else {
+    letras.push(key);
+    return true;
+  }
+}
+
+export function agregarLetraCorrecta(i) {
+  palabraCorrecta += palabraSecreta[i].toUpperCase();
+}
+
+export function agregarLetraIncorrecta(letra) {
+  if (palabraSecreta.indexOf(letra) <= 0) {
+    errores -= 1;
+  }
+}
+
+export const grueso = document.onkeydown = (e) => {
+  let letra = e.key.toUpperCase();
+  if (!verificarLetraApretada(e.key)) {
+    if (palabraSecreta.includes(letra)) {
+      console.log(letra);
+      agregarLetraCorrecta(palabraSecreta.indexOf(letra));
+      for (let i = 0; i < palabraSecreta.length; i++) {
+        if (palabraSecreta[i] === letra) {
+          dibujarLetrasCorrectas(i);
+        }
+      }
+    }
+    else{
+        if(!verificarLetraApretada(e.key)) return
+        agregarLetraIncorrecta(letra)
+        dibujarLetrasIncorrectas(letra, errores)
+    }
+  }
+};
--- js/index.js
+++ js/index.js
@@ -1,28 +1,20 @@
-import { dibujarLineas } from "./dibujarLineas.js";
+import { dibujarLineas, dibujarLetrasCorrectas, errores, palabraCorrecta } from "./dibujarLineas.js";
 
 export const tablero = document.querySelector("canvas").getContext("2d");
 
-var palabras = ["CASA", "HOGAR", "CONTROL", "VISOR", "ZORRO", "ARROZ", "AZALEA", "PERRERA", 
-"FELINO", "CANINO", "COLLAR", "LAMPARA", "CABALLO", "TORO", 
+export var palabras = ["CASA", "HOGAR", "CONTROL", "VISOR", "ZORRO", "ARROZ", "AZALEA", "PERRERA", 
+"FELINO", "CANINO", "COLLAR", "LAMPARA", "CABALLO", "TORO", "ELEMENTO", "CAJA", "ALURA",
 "MITOSIS", "PARASITO", "PROGRAMA", "DRAGON", "BEBIDA", "COMIDA", "PALABRA", "AHORCADO"];
 
-var letras = [];
-
-var palabraCorrecta = "";
-
-var errores = 8;
-
-export function contarErrores() {
-    for (let i = 0; i < errores; i++) {
-        console.log(errores);
-    };
-};
+export var letras = [];
 
 export function elegirPalabra() {
     var palabra = palabras[Math.floor(Math.random()*palabras.length)];
     return palabra;
 }
 
-export const palabraSecreta = elegirPalabra();
+export let palabraSecreta = elegirPalabra();
 
-console.log(palabraSecreta)
+console.log(palabraSecreta);
+
+dibujarLineas();
